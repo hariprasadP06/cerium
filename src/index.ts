9ddbaf1309a6303f116b382823b6d27d6c694cee
Hari prasad P <hariprasad.1si22cs404@gmail.com>
2025-03-17 11:24:07 +0530

    add axios dependency and implement POST request to mock API

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,51 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import axios from "axios";
 
-const app = new Hono()
+import { json } from "stream/consumers";
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const baseUrl = "https://a90b05cb-e593-4a77-a500-82b909c678f2.mock.pstmn.io";
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+try {
+  // const response = await axios.get(`${baseUrl}/cities`);
+
+  const response = await axios.post(`${baseUrl}/cities`, {
+    name: "Blr",
+    state: "KA",
+    country: "India",
+  },{
+    headers: {
+      "Content-Type": "application/json",
+    }
+  
+  });
+
+
+  const statusCode = response.status;
+  const data = response.data;
+  
+
+  console.log("status", statusCode);
+  console.log("Data", data);
+
+}
+catch (e) {
+  console.log("Error" , e);
+}
+
+// //JSON Stringification & JSON Parsing
+
+// const data = {
+//   name: "Hari",
+//   age: 23,
+//   city: "India",
+// };
+
+
+// // cosnt jsonStringCustom = `{"name":"Hari","age":23,"city":"India"}`;
+
+// const jsonString = JSON.stringify(data);
+
+// console.log(jsonString);
+
+
+// const parsedData = JSON.parse(jsonString);
+// console.log(parsedData);
